fe02db268b7118f742cfa26623cd1cb636e681a1
Dominik Link <vorbild94@gmail.com>
2020-06-23 21:36:24 +0200

    finished buildlink function

--- parse.go
+++ parse.go
@@ -1,7 +1,6 @@
 package link
 
 import (
-	"fmt"
 	"golang.org/x/net/html"
 	"io"
 )
@@ -20,12 +19,24 @@ func Parse(r io.Reader) ([]Link, error){
 		return nil, err
 	}
 	nodes := linkNodes(doc)
+	var links []Link
 	for _, node := range nodes{
-		fmt.Println(node)
+		links = append(links, buildLink(node))
 	}
-	return nil, nil
+	return links, nil
 }
 
+func buildLink(n *html.Node) Link {
+	var ret Link
+	for _, attr := range n.Attr {
+		if attr.Key == "href" {
+			ret.Href = attr.Val
+			break
+		}
+	}
+	ret.Text = "TODO: parse the text..."
+	return ret
+}
 func linkNodes(n *html.Node) []*html.Node {
 	if n.Type == html.ElementNode && n.Data == "a" {
 		return []*html.Node{n}
